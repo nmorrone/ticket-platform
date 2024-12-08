7058c9495849ea599b7a22ba06f0d1af90411810
nmorrone <n.morrone96@gmail.com>
2024-12-08 15:15:26 +0100

    DashboardAlmostCompleted-FirstLabelsAdminTicketCreation

--- src/main/java/it/spring/ticket/platform/controller/TicketController.java
+++ src/main/java/it/spring/ticket/platform/controller/TicketController.java
@@ -1,38 +1,111 @@
 package it.spring.ticket.platform.controller;
 
+import java.util.ArrayList;
 import java.util.List;
-
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import it.spring.ticket.platform.model.Categoria;
+import it.spring.ticket.platform.model.Stato;
 import it.spring.ticket.platform.model.Ticket;
+import it.spring.ticket.platform.model.User;
+import it.spring.ticket.platform.repository.CategorieRepository;
+import it.spring.ticket.platform.repository.NoteRepository;
+import it.spring.ticket.platform.repository.StatiRepository;
 import it.spring.ticket.platform.repository.TicketsRepository;
+import it.spring.ticket.platform.repository.UserRepository;
+import jakarta.validation.Valid;
 
 @Controller
-@RequestMapping("/tickets")
+@RequestMapping("/")
 public class TicketController {
 	
 	@Autowired
 	private TicketsRepository ticketsRepo;
+	@Autowired
+	private UserRepository userRepo;
+	@Autowired
+	private CategorieRepository categorieRepo;
+	@Autowired 
+	private StatiRepository statiRepo;
+	@Autowired 
+	private NoteRepository noteRepo;
 	
-	@GetMapping("")
+	//visualizzazione tickets ADMIN
+	@GetMapping("/dashboard-admin")
 	public String listaTickets(Authentication authentication, @RequestParam(name="keyword", required = false) String keyword,Model model) {
+	
 		
 		List <Ticket> listaTickets;
 		if(keyword!=null && !keyword.isBlank()) {
 			listaTickets = ticketsRepo.findByTitoloContaining(keyword);
+			model.addAttribute("keyword", keyword);
 		}
 		else {
 			listaTickets = ticketsRepo.findAll();
 		}
 		model.addAttribute("tickets", listaTickets);
+		return "tickets/dashboard-admin";
+		
+	}
+	
+	//rimozione Ticket dalla lista ADMIN
+	@PostMapping("/rimuovi-ticket/{id}")
+	public String rimozioneTicket(@PathVariable Integer id) {
+		ticketsRepo.deleteById(id);
+		return "redirect:/tickets";
+	}
+	//Paginainserimento nuovo ticket ADMIN
+	@GetMapping("/crea-ticket")
+	public String creazioneTicket(Model model) {
+		List <Categoria> categorie = categorieRepo.findAll();
+		List <Stato> stati = statiRepo.findAll();		
+		List <User> operatori = new ArrayList<>();
+		for(User user : userRepo.findAll()) {
+			if(user.isDisponibile() == true) {
+				operatori.add(user);
+			}
+		}
+		model.addAttribute("ticket", new Ticket());
+		model.addAttribute("operatori", operatori);
+		model.addAttribute("categorie", categorie);
+		model.addAttribute("stati", stati);
+		return "tickets/crea-ticket";
+		
+	}
+	@PostMapping("/crea-ticket")
+	public String storeTicket(@Valid @ModelAttribute ("ticket") Ticket ticketForm,BindingResult bindingResults, Model model) {
+			if(bindingResults.hasErrors()) {return "tickets/crea-ticket";}
+			ticketsRepo.save(ticketForm);
+			return "redirect:/dashboard-admin";
+	}
+	
+	//visualizzazione tickets OPERATORE
+	@GetMapping("/tickets")
+	public String listaTicketsOperatore(Authentication authentication, Model model) {
+		
+		Optional<User> loggedUser = userRepo.findByUsername(authentication.getName());
+		User operatore = loggedUser.get();
+		List <Ticket> ticketsOperatore = operatore.getTickets();
+		model.addAttribute("ticketsOperatore", ticketsOperatore);
+		model.addAttribute("operatore", operatore);
 		
 		return "tickets/lista-tickets";
 	}
+	
 
+	
+	
 }
